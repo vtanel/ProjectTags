e38c9de16205914108e10922596156240e949c7c
Tanel Viira <tanel.viira@khk.ee>
2015-09-21 17:57:49 +0300

    Kasuta kõiki teege [Delivers #103804782]

---
+++ index.php
@@ -0,0 +1,272 @@
+<!--kommenteerin siia-->
+<!doctype html>
+<html lang="et">
+<head>
+    <link rel="stylesheet" href="">
+    <meta charset="UTF-8">
+    <title>Tags</title>
+    <base href="http://www.tlu.ee/~toomas91/images/" target="_blank">
+</head>
+<body>
+<a href="https://github.com/vtanel/ProjectTags">Minu Git</a>
+
+<h1>Tartu <abbr title="Kutsehariduskeskus">KHK</abbr></h1>
+<address>
+    Aadress: Kopli 1, Tartu<br>
+    Info: info@khk.ee
+</address>
+<figure>img src="sokolaad.jpg" width="145" height="126"></figure>
+<img src="pilt.jpg" alt="suvapilt width=145" height="126" usemap="#map">
+<figcaption>Pildile kirjeldus</figcaption>
+<p><cite>sokolaad</cite> on suvaline netist v&otildeetud pilt</p>
+<map name="map">
+    <area shape="circle" coords="190,128,113" alt="KHK" href="www.khk.ee">
+</map>
+<article>
+    <h2>Projekt</h2>
+
+    <p>K&otildeik tag-id tuleb l&aumlbi proovida.</p>
+</article>
+<aside><p><b>See on aside..</b></p></aside>
+<audio controls>
+    <source src="horse.ogg" type="audio/ogg">
+    <source src="horse.mp3" type="audio/mpeg">
+</audio>
+<table>
+    <ol>
+        <li><bdo dir="rtl">Esimene n&aumlide</bdo></li>
+        <li><bdo dir="rtl">Teine n&aumlide</bdo></li>
+    </ol>
+    <ul>
+        <li>Kangelane
+            <bdi>Supermiis</bdi>
+        </li>
+        <li>Kangelane
+            <bdi>B&aumltm&aumln</bdi>
+        </li>
+        <li><big>Kangelane</big>
+            <bdi>????</bdi>
+        </li>
+    </ul>
+    <dl>
+        <dt>Piim</dt>
+        <dd>Valge jook, sisaldab kaltsiumi</dd>
+        <dt>Puder</dt>
+        <dd>Hommikus&ouml&oumlk</dd>
+    </dl>
+
+</table>
+<span>
+<blockquote cite="http://www.brainyquote.com/quotes/quotes/a/alberteins133991.html">
+    Quote - Insanity: doing the same thing over and over again and expecting different results.
+    <br> - Alber Einstein
+</blockquote>
+</span>
+
+<form action="">
+    <input type="text" list="loomad">
+    <datalist id="loomad">
+        <option value="Karu">
+        <option value="Siil">
+        <option value="Rebane">
+        <option value="Elevant">
+        <option value="Kaelkirjak">
+    </datalist>
+    <button type="button">&Aumlra vajuta siia!</button>
+</form>
+<center>
+    <table border="1">
+        <br>
+        <colgroup>
+            <col span="1" style="background-color:red">
+            <col style="background-color:yellow">
+        </colgroup>
+        <caption>Mis tyypi <dfn>tabeliga</dfn> on tegu?</caption>
+        <tr>
+            <th colspan="2">Pealkiri</th>
+        </tr>
+        <tr>
+            <td>Info1</td>
+            <td>Info2</td>
+        </tr>
+        <tr>
+            <td>Info3</td>
+            <td>Info4
+                <dialog open>INFO 4 DIALOOG</dialog>
+            </td>
+        </tr>
+    </table>
+</center>
+<p>Koodijupike</p>
+<code>Koodijupi kirjastiil123</code>
+
+<p>Joon
+    <del>teed</del>
+    , hoopis
+    <ins>kohvi</ins>
+    !
+</p>
+Detailid:
+<details>
+    <summary><em>Siia l&aumlheb mingi teema.</em></summary>
+    <p>Detail 1</p>
+
+    <p>Detail 2</p>
+</details>
+<div style="float:right; border:3px dotted red;height:20px;width:150px;">Div</div>
+<form>
+    <fieldset>
+        <legend>Tekst:</legend>
+        Nimi: <input type="text"><br>
+        Perenimi: <input type="text"><br>
+        S&uumlnniaeg: <input type="text">
+    </fieldset>
+    <i>hehe</i>
+
+    <form action="index.php">
+        <input type="text">
+        <keygen name="security">
+            <label for="sa">tadaa</label>
+    </form>
+    <kbd>Keyboard sisestus</kbd>
+    <h4>h4</h4>
+    <meter value="0.2">10%</meter>
+
+</form>
+<footer>
+    <p>
+        <ins>Footeri</ins>
+        <mark>jaoks</mark>
+    </p>
+</footer>
+<main>
+    <h1>h1</h1>
+
+    <h2>h2</h2>
+
+    <h3>h3</h3>
+
+    <h5>h5</h5>
+    <h6>h6</h6>
+</main>
+<hr>
+
+<nav><a href="">dsadas</a>
+    |<a href="">dsadas</a>
+    |<a href="">sdaasd</a>
+</nav>
+
+<object data="http://giphy.com/gifs/nyan-cat-sIIhZliB2McAo" type=""></object>
+<select>
+    <optgroup label="Kool">
+        <option value="volvo">TKHK</option>
+        <option value="saab">HKHK</option>
+    </optgroup>
+    <optgroup label="Ylikool">
+        <option value="mercedes">TTY</option>
+        <option value="audi">TY</option>
+    </optgroup>
+</select>
+
+<form oninput="x.value=parseInt(a.value)+parseInt(b.value)">0
+    <input type="range" id="a" value="50">100
+    +<input type="number" id="b" value="50">
+    =
+    <output name="x" for="a b"></output>
+</form>
+<object data="horse.wav">
+    <param name="autoplay" value="true">
+</object>
+<pre>pre on      tore asi kuna
+see fikseerib ära tyhikud ja
+uued read..
+
+
+sadsadas
+</pre>
+<progress value="42" max="100"></progress>
+<p>q tagi kasutatakse: <q>jutum2rgid</q></p>
+<ruby>
+    ?
+    <rt>
+        <rp>(</rp>
+        ???
+        <rp>)</rp>
+    </rt>
+</ruby>
+
+<p><s>kriipsuta l2bi kasutades "s" tzagi</s></p>
+<samp>dasdasdasdsada</samp>
+
+<p id="demo"></p>
+
+
+<script>
+    document.getElementById("demo").innerHTML = "TERE JS";
+</script>
+<section>
+    <h1>WWF</h1>
+
+    <p>Section osa a annab ka kuskil lehel 2ra kasutada......</p>
+</section>
+<p>Siin <strong>strong</strong>asub
+    <small>suvaline tekst mis o n tehtud "small"</small>
+</p>
+<p>neid tekste <sub>annab</sub> kasutada <sup>indeksitena</sup></p>
+<details>
+    <summary>mitu teksti siin kokku liidetud</summary>
+    <p>kuna summari ja details v6imaldab seda teha</p></details>
+
+<table>
+    <th>dsaads</th>
+    <th>23456</th>
+    <tr>
+        <td>ddsa</td>
+        <td>fdgh</td>
+        <td>gfdas</td>
+    </tr>
+    <tr>
+        <td>fdsfsd</td>
+        <td>fdsfsd</td>
+        <td>fdsfds</td>
+    </tr>
+</table>
+<table>
+    <thead>
+    <tr>lel</tr>
+    </thead>
+    <tfoot>
+    <tr>lelel</tr>
+    </tfoot>
+    <tbody>
+    <tr>blabla</tr>
+    </tbody>
+
+
+</table>
+
+<textarea name="nameless" id="none" cols="30" rows="10"></textarea>
+
+<p>kell on
+    <time>10:00</time>
+</p>
+
+<p>see on <u>paragrahv</u></p>
+
+<tt>teletype tekst</tt>
+<var>Variable tekst</var>
+
+<p>see on
+    pikkapikkapikkapikkapikkapikkapikkapikkapikkapikkapikkapikkapikkapikkapikkapikkapikkapikkapikkapik
+    <wbr>
+    kapikkapikkapikkapikkapikkapikkapikkapikkapikkapikkapikkapikkapikkapikkapikkapikkapikkapikkapikkapikkapikkapikkapikkapikkapikkapikkapikkapikkapikkapikkapikkapikkapikkapikkapikkapikkapikkapikkapikkapikkapikkakasvu
+    sone
+</p>
+<video width="320" height="240" controls>
+    <source src="https://www.youtube.com/watch?v=k0WbP2uLJPo" type="video/mp4">
+    <source src="https://www.youtube.com/watch?v=k0WbP2uLJPo" type="video/ogg">
+    <track src="subtitles_en.vtt" kind="subtitles" srclang="en" label="English">
+    <track src="subtitles_no.vtt" kind="subtitles" srclang="no" label="Norwegian">
+</video>
+</body>
+</html>
